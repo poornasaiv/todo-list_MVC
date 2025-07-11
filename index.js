8bf5dd34794ea787d40286169173ebf7fd26357a
poornasaiv <poornasaivasanta2@gmail.com>
2025-07-11 10:25:29 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,163 @@
+const APIs = (function(){
+    const URL = "http://localhost:3000";
+
+    async function addTask(task){
+        return fetch(`${URL}/todos`, {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(task)
+        }).then(res => res.json());
+    };
+
+    async function getTasks(){
+        return fetch(`${URL}/todos`).then(res => res.json());
+    };
+
+    async function deleteTask(id){
+        return fetch(`${URL}/todos/${id}`, {
+            method: "DELETE"
+        }).then(res => res.json());  
+    };
+
+    async function updateTask(id, newTitle){
+        return fetch(`${URL}/todos/${id}`, {
+            method: "PATCH",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(newTitle)
+        }).then(res => res.json());
+    };
+
+    return {addTask, getTasks, deleteTask, updateTask};
+})();
+
+
+class TodoModel{
+    }
+
+class TodoView{
+    constructor(){
+        this.form = document.querySelector(".form");
+        this.input = document.querySelector("#name");
+        this.todoList = document.querySelector(".todo-list");
+        this.todoListItem = document.querySelector(".todo-list__item");
+    }
+
+    renderTasks(tasks){
+        tasks.forEach((task) => {
+            this.todoList.appendChild(this.createTaskElement(task));
+        })
+    };
+
+    renderNewTask(task){
+        this.todoList.appendChild(this.createTaskElement(task));
+    }
+
+    createTaskElement(task){
+        const item = document.createElement("div");
+        item.setAttribute("class", "todo-list__item")
+        this.todoList.appendChild(item);
+        item.setAttribute("id", task.id)
+
+        const itemTitle = document.createElement("div");
+        itemTitle.innerText = task.title;
+        itemTitle.setAttribute("class", "todo-list__item__title");
+        item.appendChild(itemTitle);
+
+        const itemAction = document.createElement("div");
+        itemAction.setAttribute("class", "todo-list__item__actions");
+        item.appendChild(itemAction);
+
+        const editBtn = document.createElement("button");
+        editBtn.setAttribute("class", "edit");
+        editBtn.innerText = "Edit";
+        itemAction.appendChild(editBtn);
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.setAttribute("class", "delete");
+        deleteBtn.innerText = "Delete";
+        itemAction.appendChild(deleteBtn);
+
+        return item;
+    };
+
+    removeTaskElement(id){
+        document.getElementById(id).remove();
+    };
+
+    updateTask(id, newTitle){
+        document.getElementById(`${id}`).firstElementChild.innerText = newTitle;
+    };
+
+    errorAlert(){
+        alert("Enter a valid name to add the task");
+    };
+}
+
+class TodoController{
+    constructor(view, model){
+        this.view = view;
+        this.model = model;
+        this.init();
+    };
+
+    init(){
+        this.formSubmitEvent();
+        this.fetchTasks();
+        this.taskDeleteEvent();
+        this.taskUpdateEvent()
+    };
+
+    async fetchTasks(){
+        const tasks = await APIs.getTasks();
+        this.view.renderTasks(tasks);
+    };
+
+    formSubmitEvent(){
+        this.view.form.addEventListener("submit", async (e)=>{
+            e.preventDefault();
+            const name = this.view.input.value;
+            if(name!=null && name!="")
+            {
+                const title = await APIs.addTask({title : name});
+                this.view.renderNewTask(title);
+            }else{
+                this.view.errorAlert();
+            }
+            this.view.input.value = "";
+        })
+    };
+
+    taskDeleteEvent(){
+        this.view.todoList.addEventListener("click", async (e)=>{
+            if(e.target.classList.contains("delete"))
+            {
+                const id = e.target.parentElement.parentElement.getAttribute("id");
+                await APIs.deleteTask(id);
+                this.view.removeTaskElement(id);
+            }
+        })
+    };
+
+    taskUpdateEvent(){
+        this.view.todoList.addEventListener("click", async (e)=>{
+        if(e.target.classList.contains("edit")){
+            const id = e.target.parentElement.parentElement.getAttribute("id");
+            const oldTitle = e.target.parentElement.previousSibling.innerText;
+            const newTitle = prompt(`Current name : ${oldTitle}. Enter a new name to update`);
+            if(newTitle && newTitle.trim() !== "")
+            {
+            await APIs.updateTask(id, {title:newTitle});
+            this.view.updateTask(id, newTitle);
+            }
+        }
+    });
+    }
+};
+
+const model = new TodoModel();
+const view = new TodoView();
+const controller = new TodoController(view, model);
